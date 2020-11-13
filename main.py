e0d88444d80380edf6c51a798e5b82cbd64838de
Joshua <33937944+Stylos21@users.noreply.github.com>
2020-11-13 08:00:27 -0500

    Update main.py
    NOT YET DONE

--- main.py
+++ main.py
@@ -18,16 +18,14 @@ square_pos = [
     [0.6, 0.8, 0.6, 0.8],
 ]
 
+ticTacs = [False for _ in range(9)]
+
+
+def draw_rects(surface, arr):
+    pass
+
+
 while isRunning:
-    board.fill((0, 0, 0))
-    pygame.draw.line(board, (255, 255, 255), (500*0.4, 500*0.2), (500*.4, 500*.8))
-    pygame.draw.line(board, (255, 255, 255), (500*0.6, 500*0.2), (500*.6, 500*.8))
-
-    pygame.draw.line(board, (255, 255, 255), (500 * 0.2, 500 * 0.4), (500 * .8, 500 * .4))
-    pygame.draw.line(board, (255, 255, 255), (500 * 0.2, 500 * 0.6), (500 * .8, 500 * .6))
-
-    pygame.display.update()
-
 
     pos = pygame.mouse.get_pos()
     x = pos[0] / 500
@@ -38,8 +36,17 @@ while isRunning:
         if event.type == pygame.QUIT:
             isRunning = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for p, i in enumerate(square_pos):
-                if i[0] < x < i[1] and i[2] < y < i[3]:
-                    print(p)
+        board.fill((0, 0, 0))
+        pygame.draw.line(board, (255, 255, 255), (500 * 0.4, 500 * 0.2), (500 * .4, 500 * .8))
+        pygame.draw.line(board, (255, 255, 255), (500 * 0.6, 500 * 0.2), (500 * .6, 500 * .8))
 
+        pygame.draw.line(board, (255, 255, 255), (500 * 0.2, 500 * 0.4), (500 * .8, 500 * .4))
+        pygame.draw.line(board, (255, 255, 255), (500 * 0.2, 500 * 0.6), (500 * .8, 500 * .6))
+        # pygame.display.update()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(x, y)
+            for p, i in enumerate(square_pos):
+                if i[0] < x < i[1] and i[2] < y < i[3] and not ticTacs[p]:
+                    draw_rects(board, ticTacs)
+        pygame.display.update()
